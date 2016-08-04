ccbf3a4995e5f659a6b2e1d6c30c89f0a9438ee3
IRelaxxx <alexanderkrahl@hotmail.de>
2016-08-04 18:29:45 +0200

    Initial Commit

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipController : MonoBehaviour {
+
+	bool turnsRight = false;
+	Rigidbody2D rb;
+
+	public int MoveSpeed;
+	public GameObject Sub;
+	public Transform SubHolder;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			ReleaseSub ();
+		}
+	}
+
+	void FixedUpdate(){
+		float x = Input.GetAxis ("Horizontal") * Time.fixedDeltaTime;
+
+		if(x > 0){
+			turnsRight = true;
+		}
+		else{
+			turnsRight = false;
+		}
+		rb.velocity = new Vector2 (x, 0).normalized * MoveSpeed;
+		print (new Vector2 (x, 0).normalized);
+	}
+
+	void ReleaseSub(){
+		Sub.transform.SetParent (null, true);
+		Camera.main.gameObject.transform.SetParent (Sub.transform);
+		Sub.transform.position -= Vector3.down * 2;
+		Sub.GetComponent<SubmarineController> ().enabled = true;
+		this.enabled = false;
+	}
+}
---
+++ Assets/Scripts/SubmarineController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SubmarineController : MonoBehaviour {
+
+	bool turnsRight = false;
+	Rigidbody2D rb;
+
+	public int MoveSpeed;
+	public GameObject Ship;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dist = Vector3.Distance (transform.position, Ship.transform.position);
+		if(dist < 3){
+			DockAtShip ();
+		}
+	}
+
+	void FixedUpdate(){
+		float x = Input.GetAxis ("Horizontal") * Time.fixedDeltaTime;
+		float y = Input.GetAxis ("Vertical") * Time.fixedDeltaTime;
+
+		if(y > 0 && transform.position.y >= 0){
+			y = 0;
+		}
+		if(x > 0){
+			turnsRight = true;
+		}
+		else{
+			turnsRight = false;
+		}
+		rb.velocity = new Vector2 (x, y).normalized * MoveSpeed;
+	}
+
+	void DockAtShip(){
+		transform.SetParent (Ship.transform, true);
+		Camera.main.gameObject.transform.SetParent (Ship.transform);
+		Ship.GetComponent<ShipController> ().enabled = true;
+		transform.position = Ship.GetComponent<ShipController> ().SubHolder.position;
+		this.enabled = false;
+	}
+}
