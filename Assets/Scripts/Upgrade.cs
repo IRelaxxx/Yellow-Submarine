eeecf1213f4ace3496eadd2a40e2d64fff637d94
DoppelpunktDeeehhh <basecap@t-online.de>
2016-08-12 22:49:10 +0200

    Shit der nicht funktioniert wie er soll

---
+++ Assets/Scripts/Upgrade.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Upgrade : MonoBehaviour
+{
+    public GameObject Panel;
+    public GameObject House;
+    public bool PausePanel = false;
+
+
+
+
+
+    void Update()
+    {
+        if (PausePanel == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                Panel.SetActive(true);
+                PausePanel = true;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Panel.SetActive(false);
+            PausePanel = false;
+        }
+        
+
+    }
+}
