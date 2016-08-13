e9f1f3bd897d0aa6f273fa9f1b6d7be275be5a00
IRelaxxx <alexanderkrahl@hotmail.de>
2016-08-13 11:01:37 +0200

    Work on Upgrades

--- Assets/Scripts/Upgrade.cs
+++ Assets/Scripts/Upgrade.cs
@@ -1,32 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class Upgrade : MonoBehaviour
 {
     public GameObject Panel;
-    public GameObject House;
-    public bool PausePanel = false;
-
-
-
-
+	public Transform UpdatePoint;
+	public Transform Ship;
 
     void Update()
     {
-        if (PausePanel == false)
+		if (!Panel.activeSelf)
         {
-            if (Input.GetKeyDown(KeyCode.Tab))
-            {
-                Panel.SetActive(true);
-                PausePanel = true;
-            }
+			if (Vector3.Distance (Ship.position, UpdatePoint.position) < 10) {
+				if (Input.GetKeyDown (KeyCode.Tab)) {
+					Panel.SetActive (true);
+					Time.timeScale = 0;
+				}
+			}
         }
         else if (Input.GetKeyDown(KeyCode.Tab))
         {
             Panel.SetActive(false);
-            PausePanel = false;
+			Time.timeScale = 1;
         }
-        
-
     }
+
+	class UpgradeDta{
+		public string Text ="";
+		public int Cost = 0;
+		public Sprite Icon = null;
+		public Action<UpgradeDta> OnUnlock = null;
+
+		public UpgradeDta(string text, int cost, Sprite icon, Action<UpgradeDta> onUnlock){
+			Text = text;
+			Cost = cost;
+			Icon = icon;
+			OnUnlock = onUnlock;
+		}
+	}
+
+	UpgradeDta[] UpgradeList = {
+		new UpgradeDta("10 More Pressure Max", 200, null, (u) => {Stats.Instance.maxPres += 10;})
+	};
+
+	void ProcessUpdate(GameObject button, UpgradeDta upgrade){
+
+	}
 }
