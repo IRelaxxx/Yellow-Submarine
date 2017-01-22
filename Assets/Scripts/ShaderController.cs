560a323ddbd2a8ff9d08e328088af7c0881ba487
IRelaxxx <alexanderkrahl@hotmail.de>
2017-01-22 19:44:31 +0100

    Inverted, changes to soon

---
+++ Assets/Scripts/ShaderController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderController : MonoBehaviour
+{
+    private GameObject sub;
+    private Material m;
+	void Start ()
+    {
+        sub = GameObject.FindObjectOfType<SubmarineController>().gameObject;
+        m = GetComponent<SpriteRenderer>().material;
+	}
+	
+	void Update ()
+    {
+        m.SetFloat("_Alpha", 1 / Mathf.Abs(sub.transform.position.y));
+	}
+}
